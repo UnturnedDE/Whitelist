afb898856f4b467e492ea9925b0e27bcba0c6e12
TimoGuentner <Timo_Guentner@gmx.de>
2015-04-07 00:26:26 +0200

    Update to Rocket 3.2.1.0

--- Whitelist/Whitelist.cs
+++ Whitelist/Whitelist.cs
@@ -9,25 +9,26 @@ using UnityEngine;
 using System.Net;
 using Rocket.RocketAPI.Events;
 using Rocket.Logging;
+using Steamworks;
 
 namespace Whitelist
 {
     public class Whitelist : RocketPlugin
     {
-        WebClient webClient = new WebClient();
-
         protected override void Load()
         {
             RocketServerEvents.OnPlayerConnected += Events_OnPlayerConnected;
         }
 
-        void Events_OnPlayerConnected(Player player)
+        void Events_OnPlayerConnected(RocketPlayer player)
         {
-            SteamPlayerID steamId = player.SteamChannel.SteamPlayer.SteamPlayerID;
-            string check = webClient.DownloadString("http://unturned.de/steam.php?steamId=" + steamId.CSteamID.ToString());
-            if (check != "true")
+            WebClient webClient = new WebClient();
+
+            CSteamID cSteamId = player.CSteamID;
+            string whitelisted = webClient.DownloadString("http://unturned.de/steam.php?steamId=" + cSteamId.ToString());
+            if (whitelisted != "true")
             {
-                Steam.kick(steamId.CSteamID, "Bitte registriere dich auf http://unturned.de/, um dem Server joinen zu können");
+                player.Kick("Bitte registriere dich auf http://unturned.de/, um dem Server joinen zu können");
             }
         }
     }
